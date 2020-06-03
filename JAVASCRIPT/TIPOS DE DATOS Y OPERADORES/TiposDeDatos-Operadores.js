83abeaf0ee93b33e86b06cf0fccad1726705fae6
alanmartinc <alancabot.ac@gmail.com>
2020-06-02 23:39:15 -0300

    Se agrega ejercicio tipos de datos y operadores

---
+++ JAVASCRIPT/TIPOS DE DATOS Y OPERADORES/TiposDeDatos-Operadores.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// Tipos de datos
+var numero_entero = 44;
+var cadena_texto = "Hola que tal";
+var verdadero_o_falso = true;
+var numero_falso = "33";
+
+console.log(parseInt(numero_falso)+ 7);
+console.log(String(numero_entero)+ 4);
+console.log(typeof verdadero_o_falso);
+
+// Operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 + numero2;
+
+alert("El resultado de la operacion es: " + operacion);
