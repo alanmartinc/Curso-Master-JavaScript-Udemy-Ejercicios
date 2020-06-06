c4c47188d3c25374c4ba2b73841a536d9648a43b
alanmartinc <alancabot.ac@gmail.com>
2020-06-06 02:10:01 -0300

    Se agrega ejercicio alertas y ventanas

---
+++ JAVASCRIPT/ALERTAS Y VENTANAS/Alertas-Ventanas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// Alerta
+alert("Esta es mi alerta!");
+
+// Confirmacion
+var mi_resultado1 = confirm("¿Estas seguro de querer continuar?");
+console.log(mi_resultado1);
+
+// Ingreso de datos
+var mi_resultado2 = prompt("¿Que edad tienes?", 18);
+console.log(mi_resultado2);
