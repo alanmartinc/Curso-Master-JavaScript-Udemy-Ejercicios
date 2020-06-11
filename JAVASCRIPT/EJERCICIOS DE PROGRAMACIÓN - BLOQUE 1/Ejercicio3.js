0f570ef858e11e0a182bef5ab048dab67946874e
alanmartinc <alancabot.ac@gmail.com>
2020-06-11 01:10:50 -0300

    Se agrega ejercicio 3

---
+++ JAVASCRIPT/EJERCICIOS DE PROGRAMACIÓN - BLOQUE 1/Ejercicio3.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/* Hacer un programa que muestre todos los numeros entre dos numeros introducidos por el usuario */
+
+var numero1 = parseInt(prompt("Introduce el primer numero", 0));
+var numero2 = parseInt(prompt("Introduce el segundo numero", 0));
+
+document.write("<h1>De " + numero1 + " a " + numero2 + " estan estos numeros:</h1>")
+
+for(var i = numero1; i <= numero2; i++){
+	document.write(i + "<br>");
+}
