7de70cc257ca1922c8a6c93aae35b195106f4a6d
alanmartinc <alancabot.ac@gmail.com>
2020-06-13 01:19:56 -0300

    Se agrega ejercicio 5

---
+++ JAVASCRIPT/EJERCICIOS DE PROGRAMACIÓN - BLOQUE 1/Ejercicio5.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/* Muestre todos los numeros divisores de un numero introducido en prompt */
+
+var numero = parseInt(prompt("Mete un numero", 1));
+
+for(var i = 1; i <= numero; i++){
+
+	if(numero%i == 0){
+		console.log("Divisor: " + i);
+	}
+}
