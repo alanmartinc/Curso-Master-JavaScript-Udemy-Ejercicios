ee40988d80181a54a99c751302f9db53b75282b0
alanmartinc <alancabot.ac@gmail.com>
2020-06-18 22:14:45 -0300

    Se agrega ejercicio crear funciones

---
+++ JAVASCRIPT/FUNCIONES Y MÉTODOS/Funciones-metodos.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// Funciones
+
+function calculadora(){
+	console.log("Hola soy la calculadora!!");
+	return("Si soy yo");
+}
+
+var resultado = calculadora();
+console.log(resultado);
