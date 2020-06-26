941d3e46e21a84b88650f18a3e91826a38cab2d1
alanmartinc <alancabot.ac@gmail.com>
2020-06-25 23:35:02 -0300

    Se agrega ejercicio funciones flecha

---
+++ JAVASCRIPT/FUNCIONES Y MÉTODOS/Funciones-Flecha.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function sumame(numero1, numero2, sumaYmuestra, sumaPorDos){
+	var sumar = numero1 + numero2;
+
+	sumaYmuestra(sumar);
+	sumaPorDos(sumar);
+
+	return sumar;
+}
+
+sumame(5, 7, dato =>{
+	console.log("La suma es:", dato);
+},
+function(dato){
+	console.log("La suma por dos es:", (dato*2));
+});
