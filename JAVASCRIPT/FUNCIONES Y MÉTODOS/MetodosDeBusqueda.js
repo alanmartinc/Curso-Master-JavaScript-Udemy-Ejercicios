8f6233348db077763cfbbb26b4e9329aa3b125b2
alanmartinc <alancabot.ac@gmail.com>
2020-06-28 23:09:28 -0300

    Se agrega ejercicio metodos de busqueda

---
+++ JAVASCRIPT/FUNCIONES Y MÉTODOS/MetodosDeBusqueda.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var numero = 444;
+var texto1 = "Bienvenido al curso de JavaScript de Victor Robles";
+var texto2 = "es muy buen curso";
+
+var dato = numero.toString();
+	dato = texto1.toLowerCase();
+	dato = texto2.toUpperCase();
+
+var nombre = "Victor Robles";
+var textoTotal = texto1.concat(" " + texto2);
+
+console.log(textoTotal);
