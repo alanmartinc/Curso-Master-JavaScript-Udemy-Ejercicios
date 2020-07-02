c846bb5f684828f3f6f8026a5e3fe1660dcb32e4
alanmartinc <alancabot.ac@gmail.com>
2020-07-01 23:21:25 -0300

    Se agrega ejercicio funciones de reemplazo

---
+++ JAVASCRIPT/FUNCIONES Y MÉTODOS/FuncionesDeReemplazo.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var numero = 444;
+var texto1 = "Bienvenido al curso de JavaScript de Victor Robles";
+var texto2 = "es muy buen curso";
+
+var busqueda = texto1.trim();
+console.log(busqueda);
