a596479aead7a567a0a6f95844feb6dc8cb660e8
alanmartinc <alancabot.ac@gmail.com>
2020-07-12 23:46:04 -0300

    Se agrega ejercicio arrays basicos

---
+++ JAVASCRIPT/ARRAYS Y ARREGLOS/ArraysBasicos.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var nombre = "Victor Robles";
+var nombres = ["Victor Robles", "Juan Lopez", "Manolo Garcia", "Jose Martin", 52, true];
+
+var lenguajes = new Array("PHP", "JS", "Go", "Java");
+
+console.log(nombres[0]);
