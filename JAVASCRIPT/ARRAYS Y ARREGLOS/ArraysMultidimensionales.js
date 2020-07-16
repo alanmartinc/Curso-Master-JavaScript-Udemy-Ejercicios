dfb6756c82d6dc565784de3258b5f9472bbe3436
alanmartinc <alancabot.ac@gmail.com>
2020-07-15 22:27:14 -0300

    Se agrega ejercicio arrays multidimensionales

---
+++ JAVASCRIPT/ARRAYS Y ARREGLOS/ArraysMultidimensionales.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var categorias = ['Acción', 'Terror', 'Comedia'];
+var peliculas = ['La verdad duele', 'La vida es bella', 'Gran torino'];
+
+var cine = [categorias, peliculas];
+
+console.log(cine[0][1]);
+console.log(cine[1][2]);
