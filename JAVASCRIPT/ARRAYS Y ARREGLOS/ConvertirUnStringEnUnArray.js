f203d40b275feea76a76cae3e021edec951202c4
alanmartinc <alancabot.ac@gmail.com>
2020-07-17 23:40:13 -0300

    Se agrega ejercicio convertir un string en un array

--- JAVASCRIPT/ARRAYS Y ARREGLOS/ConvertirUnStringEnUnArray.js
+++ JAVASCRIPT/ARRAYS Y ARREGLOS/ConvertirUnStringEnUnArray.js
@@ -3,10 +3,6 @@
 var categorias = ['Acción', 'Terror', 'Comedia'];
 var peliculas = ['La verdad duele', 'La vida es bella', 'Gran torino'];
 
-peliculas.reverse();
-
-console.log(peliculas);
-
 var cine = [categorias, peliculas];
 
 var indice = peliculas.indexOf('Gran torino');
@@ -14,3 +10,10 @@ var indice = peliculas.indexOf('Gran torino');
 if(indice > -1){
     peliculas.splice(indice, 1);
 }
+
+var peliculas_string = peliculas.join();
+
+var cadena = "texto1, texto2, texto3";
+var cadena_array = cadena.split(", ");
+
+console.log(cadena_array);
