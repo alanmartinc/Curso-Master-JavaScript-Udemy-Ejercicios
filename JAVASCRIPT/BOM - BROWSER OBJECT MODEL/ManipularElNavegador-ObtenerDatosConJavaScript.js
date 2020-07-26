d3fce99791edb3b7febe3ff94a08045fda3176ca
alanmartinc <alancabot.ac@gmail.com>
2020-07-25 23:49:45 -0300

    Se agrega ejercicio BOM - browser object model

---
+++ JAVASCRIPT/BOM - BROWSER OBJECT MODEL/ManipularElNavegador-ObtenerDatosConJavaScript.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function getBom(){
+    console.log(screen.width);
+    console.log(screen.height);
+    console.log(window.location);
+}
+
+function redirect(url){
+    window.location.href = url;
+}
+
+function abrirVentana(url){
+    window.open(url,"","width=400,height=300");
+}
+
+getBom();
