bffc9d981e7d085164b77334a917f3586e99029e
alanmartinc <alancabot.ac@gmail.com>
2020-07-30 22:44:59 -0300

    Se agrega ejercicio timers

---
+++ JAVASCRIPT/EVENTOS/Timers.js
@@ -0,0 +1,37 @@
+'use strict'
+
+window.addEventListener('load', () => {
+
+    function intervalo() {
+        // Timers
+        var tiempo = setInterval(function(){
+            console.log("Set interval ejecutado");
+
+            var encabezado = document.querySelector("h1")
+            if(encabezado.style.fontSize == "50px"){
+                encabezado.style.fontSize = "30px";
+            } else{
+                encabezado.style.fontSize = "50px";
+            }
+        }, 2000);
+
+        return tiempo;
+    }
+
+    var tiempo = intervalo();
+
+    var stop = document.querySelector("#stop");
+
+    stop.addEventListener("click", function(){
+        alert("Has parado el intervalo en bucle");
+        clearInterval(tiempo);
+    });
+
+    var start = document.querySelector("#start");
+
+    start.addEventListener("click", function(){
+        alert("Has iniciado el intervalo en bucle");
+        intervalo();
+    });
+});
+
