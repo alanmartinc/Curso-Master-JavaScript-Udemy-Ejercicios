b796571df5a0de5d96d471121ba7de408595cbef
alanmartinc <alancabot.ac@gmail.com>
2020-08-08 23:20:02 -0300

    Se agrega ejercicio promesas fetch

---
+++ JAVASCRIPT/PETICIONES ASINCRONAS (FETCH, AJAX) Y PROMESAS/Promesas-Fetch.js
@@ -0,0 +1,52 @@
+'use strict'
+
+var div_usuarios = document.querySelector("#usuarios");
+var div_janet = document.querySelector("#janet");
+
+    getUsuarios()
+    .then(data => data.json())
+    .then(users => {
+        listadoUsuarios(users.data);
+
+        return getJanet();
+    })
+    .then(data => data.json())
+    .then(user => {
+        mostrarJanet(user.data);
+    });
+
+function getUsuarios(){
+    return fetch('https://reqres.in/api/users');
+}
+
+function getJanet(){
+    return fetch('https://reqres.in/api/users/2');
+}
+
+function listadoUsuarios(usuarios){
+    usuarios.map((user, i) => {
+        let nombre = document.createElement('h2');
+
+        nombre.innerHTML = i + ". " + user.first_name + " " + user.last_name;
+
+        div_usuarios.appendChild(nombre);
+
+        document.querySelector(".loading").style.display = "none";
+    });
+}
+
+function mostrarJanet(user){
+    let nombre = document.createElement('h4');
+    let avatar = document.createElement('img');
+
+    nombre.innerHTML = user.first_name + " " + user.last_name;
+
+    avatar.src = user.avatar;
+    avatar.width = "100";
+
+    div_janet.appendChild(nombre);
+    div_janet.appendChild(avatar);
+
+    document.querySelector("#janet .loading").style.display = "none";
+}
+
