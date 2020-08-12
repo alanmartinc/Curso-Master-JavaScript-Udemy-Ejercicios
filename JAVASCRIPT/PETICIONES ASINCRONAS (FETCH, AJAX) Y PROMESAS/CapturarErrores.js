50abd12aa899076ae85054cd34b43ad36f4be69b
alanmartinc <alancabot.ac@gmail.com>
2020-08-11 22:06:06 -0300

    Se agrega ejercicio capturar errores

---
+++ JAVASCRIPT/PETICIONES ASINCRONAS (FETCH, AJAX) Y PROMESAS/CapturarErrores.js
@@ -0,0 +1,12 @@
+'use strict'
+
+try {
+    var year = 2019;
+    alert(year);
+    var vector = new Array(999999999999999);
+    console.log(decodeURIComponent("https://victor%roblesweb.es"));
+} catch(error) {
+    console.log(error);
+    alert("A ocurrido un error en el código");
+}
+
