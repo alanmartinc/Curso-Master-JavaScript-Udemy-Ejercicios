e65d2b9414073252cd20c2e97f618214701aba3a
alanmartinc <alancabot.ac@gmail.com>
2020-08-19 22:29:22 -0300

    Se agrega ejercicio selector de ID

---
+++ JAVASCRIPT/SELECTORES/Selector-ID.js
@@ -0,0 +1,11 @@
+'use strict'
+
+$(document).ready(function() {
+	// Selector de ID
+	var rojo = $("#rojo").css("background","red")
+						 .css("color","white");
+	
+	var amarillo = $("#amarillo").css("background","yellow")
+						 		 .css("color","black");
+});
+
