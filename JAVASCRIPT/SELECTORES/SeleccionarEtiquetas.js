5ab4972a7c76606be47f2ca9dbfeb4c5e7fb1217
alanmartinc <alancabot.ac@gmail.com>
2020-08-21 22:51:24 -0300

    Se agrega ejercicio seleccionar etiquetas

---
+++ JAVASCRIPT/SELECTORES/SeleccionarEtiquetas.js
@@ -0,0 +1,32 @@
+'use strict'
+
+$(document).ready(function() {
+	// Selector de ID
+	var rojo = $("#rojo").css("background","red")
+						 .css("color","white");
+	
+	var amarillo = $("#amarillo").css("background","yellow")
+								  .css("color","black");
+	
+	// Selectores de clases
+	var mi_clase = $('.zebra').css("padding", "5px");
+
+	$('.sin_borde').click(function(){
+		$(this).addClass('zebra');
+	});
+
+	// Selectores de etiqueta
+	var parrafos = $('p').css("cursor","pointer");
+
+	parrafos.click(function(){
+		var that = $(this);
+		if(!that.hasClass('grande')){
+			that.addClass('grande');
+		} else {
+			that.removeClass('grande');
+		}
+	});
+});
+
+
+
