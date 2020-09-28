1e38b01c13cd439a4345873881f3a586658e76bc
alanmartinc <alancabot.ac@gmail.com>
2020-09-28 08:42:48 -0300

    Se agrega ejercicios metodo get - AJAX

--- PETICIONES AJAX EN JQUERY/load.js
+++ PETICIONES AJAX EN JQUERY/load.js
@@ -2,7 +2,14 @@
 
 $(document).ready(function(){
     // Load
-    $("#datos").load("https://reqres.in/");
+    // $("#datos").load("https://reqres.in/");
+
+    // Get y Post
+    $.get("https://reqres.in/api/users", {page: 3}, function(response){
+        response.data.forEach((element, index) => {
+            $("#datos").append("<p>"+element.first_name+ " " +element.last_name+"</p>");
+        });
+    });
 });
 
 
