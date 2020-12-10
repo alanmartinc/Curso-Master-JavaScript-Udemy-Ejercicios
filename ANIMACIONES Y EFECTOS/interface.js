0bad0dffc3e238c2b380134c04438a804dcc242c
alanmartinc <alancabot.ac@gmail.com>
2020-12-09 22:18:44 -0300

    Se agrega ejercicios efectos de JQuery UI

---
+++ ANIMACIONES Y EFECTOS/interface.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    
+    // Mover elemento por la página
+    $(".elemento").draggable();
+
+    // Redimensionar
+    $(".elemento").resizable();
+
+    // Seleccionar y ordenar elementos
+    // $(".lista-seleccionable").selectable();
+    $(".lista-seleccionable").sortable({
+        update: function(event, ui){
+            console.log("Ha cambiado la lista");
+        }
+    });
+
+    // Drop
+    $("#elemento-movido").draggable();
+    $("#area").droppable({
+        drop: function(){
+            console.log("Has soltado algo dentro de el area");
+        }
+    });
+
+    // Efectos
+    $("#mostrar").click(function(){
+        $(".caja-efectos").toggle("shake", 4000);
+    });
+});
