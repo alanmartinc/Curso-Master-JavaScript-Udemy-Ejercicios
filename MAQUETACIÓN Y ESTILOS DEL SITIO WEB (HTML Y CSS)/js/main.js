cf67748a996110d28f7733f13984d7cab5b4bab2
alanmartinc <alancabot.ac@gmail.com>
2021-04-28 17:41:32 -0300

    Se agrega ejercicios cambiador de temas del sitio web

--- MAQUETACIÓN Y ESTILOS DEL SITIO WEB (HTML Y CSS)/js/main.js
+++ MAQUETACIÓN Y ESTILOS DEL SITIO WEB (HTML Y CSS)/js/main.js
@@ -44,4 +44,19 @@ $(document).ready(function() {
 
 		$("#posts").append(post);
 	});
+
+	// Selector de tema
+	var theme = $("#theme");
+
+	$("#to-green").click(function() {
+		theme.attr("href", "css/green.css")
+	});
+
+	$("#to-red").click(function() {
+		theme.attr("href", "css/red.css")
+	});
+
+	$("#to-blue").click(function() {
+		theme.attr("href", "css/blue.css")
+	});
 });
