31c3bd865284dab5958e29d4d5c28a5e268f4338
alanmartinc <alancabot.ac@gmail.com>
2021-04-28 16:57:04 -0300

    Se agrega ejercicios listado dinámico de artículos / posts

--- MAQUETACIÓN Y ESTILOS DEL SITIO WEB (HTML Y CSS)/js/main.js
+++ MAQUETACIÓN Y ESTILOS DEL SITIO WEB (HTML Y CSS)/js/main.js
@@ -1,9 +1,47 @@
 $(document).ready(function() {
+	// Slider
 	$('.bxslider').bxSlider({
-	    mode: 'fade',
-	    captions: true,
-	    slideWidth: 1200,
-	    responsive: true,
-	    pager: true
-  	});
+		mode: 'fade',
+		captions: true,
+		slideWidth: 1200,
+		responsive: true,
+		pager: true
+	});
+
+	// Posts
+	var posts = [
+		{
+			title: 'Prueba de titulo 1',
+			date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+			content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis pariatur possimus quidem. Facere soluta commodi recusandae eligendi deleniti debitis cumque ullam, perferendis dolorum ducimus odio temporibus. Enim sequi libero excepturi.'
+		},
+		{
+			title: 'Prueba de titulo 2',
+			date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+			content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis pariatur possimus quidem. Facere soluta commodi recusandae eligendi deleniti debitis cumque ullam, perferendis dolorum ducimus odio temporibus. Enim sequi libero excepturi.'
+		},
+		{
+			title: 'Prueba de titulo 3',
+			date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+			content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis pariatur possimus quidem. Facere soluta commodi recusandae eligendi deleniti debitis cumque ullam, perferendis dolorum ducimus odio temporibus. Enim sequi libero excepturi.'
+		},
+		{
+			title: 'Prueba de titulo 4',
+			date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+			content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis pariatur possimus quidem. Facere soluta commodi recusandae eligendi deleniti debitis cumque ullam, perferendis dolorum ducimus odio temporibus. Enim sequi libero excepturi.'
+		},
+	];
+
+	posts.forEach((item, index) => {
+		var post = `
+		<article class="post">
+			<h2>${item.title}</h2>
+			<span class="date">${item.date}</span>
+			<p>${item.content}</p>
+			<a href="#" class="button-more">Leer más</a>
+		</article>
+		`;
+
+		$("#posts").append(post);
+	});
 });
